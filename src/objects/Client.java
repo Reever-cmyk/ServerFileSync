5c1e4478188d2bbad5a741c126ad74d5aaa5a668
Reever-cmyk <tim48@gmx.de>
2022-01-30 12:57:14 +0100

    Checksum vergleich und Client_Checksum_Sender geschrieben

--- src/objects/Client.java
+++ src/objects/Client.java
@@ -3,8 +3,13 @@ package objects;
 import fileactions.Checksum;
 import fileactions.FileStream;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.HashMap;
 
@@ -34,5 +39,40 @@ public class Client {
      */
     public InetAddress getClientIp() throws UnknownHostException {
         return InetAddress.getLocalHost();
+    }
+
+    /**
+     * sendet checksum byte für byte and Server
+     * @param port Server Port
+     * @throws IOException Sockets werfen exception
+     */
+    public void Checksum_Sender(int port) throws IOException
+    {
+        ServerSocket servSock = new ServerSocket(port);
+        Socket socket = servSock.accept();
+
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+
+        while (true)
+        {
+            dos.write(checkSum);
+            for (byte b : checkSum) {
+                dos.writeByte(b);
+            }
+            if (dis.readUTF().equals("success"))
+            {
+                System.out.println("Erfolgreiche Übertragung.");
+                break;
+            }
+            else if (dis.readUTF().equals("failure"))
+            {
+                System.out.println("Übertragung fehlgeschlagen.");
+                break;
+            }
+        }
+        dis.close();
+        dos.close();
+        socket.close();
     }
 }
